f3bef233a5ae12b7a41c999f509c4c33e4c91021
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-04-13 16:52:38 +0300

    print Vorontsov lighthouse coordinates

---
+++ src/main/java/ua/hillel/malynka/lessons/lesson2/Coordinates.java
@@ -0,0 +1,12 @@
+package ua.hillel.malynka.lessons.lesson2;
+
+public class Coordinates {
+    public static void main(String[] args) {
+        double doubleLatitude = 46.496525;
+        double doubleLongitude = 30.760007;
+        System.out.println("Vorontsov Lighthouse coordinates:");
+        System.out.println("Latitude: "+doubleLatitude);
+        System.out.println("Longitude: "+doubleLongitude);
+        System.out.println("DMS Lat: 46°29'47.5'' N"+"DMS Long: 30°45'34.4'' E");
+    }
+}
