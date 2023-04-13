3c3a386647ded475a72f0d06ade0574dea0f8575
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-04-13 19:26:16 +0300

    learning Class Math

---
+++ src/main/java/ua/hillel/malynka/lessons/lesson4/Main.java
@@ -0,0 +1,21 @@
+package ua.hillel.malynka.lessons.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(5+5);
+        System.out.println(5-5);
+        System.out.println(5*5);
+        System.out.println(5/5);
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println((double) a/b);
+        System.out.println((double) a/(double)b);
+        System.out.println(a/(double)b);
+        System.out.println();
+
+
+
+    }
+}
