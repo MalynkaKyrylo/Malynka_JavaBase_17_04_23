d31ec0ae4295dd12138963adb51e70bde451e922
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-04-13 17:00:25 +0300

    learning Variables

---
+++ src/main/java/ua/hillel/malynka/lessons/lesson2/Main.java
@@ -0,0 +1,38 @@
+package ua.hillel.malynka.lessons.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+//        byte age = 28;
+//        System.out.println(age);
+//        int intVariable = 2_000_000_111;
+//        System.out.println(intVariable);
+//        long longVariable = 4_000_000_000_000_444L;
+//        System.out.println(longVariable);
+//        float floatVariable = 77.12345678901234567890F;
+//        System.out.println(floatVariable);
+//        double doubleVariable = 88.12345678901234567890;
+//        System.out.println(doubleVariable);
+//        char symbolA = 'Ё';
+//        char symbolB = 'Й';
+//        char symbolC = 'Ы';
+//        char symbolD = 'Э';
+//        char symbolE = 'Є';
+//        char symbolF = 'Ї';
+//
+//        System.out.println(symbolA);
+//        System.out.println(symbolB);
+//        System.out.println(symbolC);
+//        System.out.println(symbolD);
+//        System.out.println(symbolE);
+//        System.out.println(symbolF);
+//
+//        boolean booleanVariableTrue = true;
+//        boolean booleanVariableFalse = false;
+//        System.out.println(booleanVariableTrue);
+//        System.out.println(booleanVariableFalse);
+
+        String str = "hello";
+        System.out.println(str + " World");
+
+    }
+}
