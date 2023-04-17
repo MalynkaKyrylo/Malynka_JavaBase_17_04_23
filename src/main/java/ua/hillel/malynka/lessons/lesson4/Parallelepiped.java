df349532813f1bc89623294c140a8d3493c9a307
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-04-17 18:19:56 +0300

    task 4 -Volume and perimeter of rectangular Parallelepiped

---
+++ src/main/java/ua/hillel/malynka/lessons/lesson4/Parallelepiped.java
@@ -0,0 +1,18 @@
+package ua.hillel.malynka.lessons.lesson4;
+
+public class Parallelepiped {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int c = 8;
+        int volume = a*b*c;
+        int perimeter= (a+b+c)*4;
+        System.out.println("Rectangular Parallelepiped with:");
+        System.out.println("Side a = "+a);
+        System.out.println("Side b = "+b);
+        System.out.println("Side c = "+c);
+        System.out.println("Volume of Parallelepiped = "+volume);
+        System.out.println("Perimeter of Parallelepiped = "+perimeter);
+
+    }
+}
