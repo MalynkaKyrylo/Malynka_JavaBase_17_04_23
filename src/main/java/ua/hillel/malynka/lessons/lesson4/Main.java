3c8de623e5d8862af26b24bf8f3f77f3c58fc30a
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-04-17 17:45:28 +0300

    formula Random number in given interval

--- src/main/java/ua/hillel/malynka/lessons/lesson4/Main.java
+++ src/main/java/ua/hillel/malynka/lessons/lesson4/Main.java
@@ -2,18 +2,123 @@ package ua.hillel.malynka.lessons.lesson4;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(5+5);
-        System.out.println(5-5);
-        System.out.println(5*5);
-        System.out.println(5/5);
+//        System.out.println(5+5);
+//        System.out.println(5-5);
+//        System.out.println(5*5);
+//        System.out.println(5/5);
+//
+//        int a = 10;
+//        int b = 3;
+//
+//        System.out.println((double) a/b);
+//        System.out.println((double) a/(double)b);
+//        System.out.println(a/(double)b);
+//        System.out.println((double) 10/3);
+//        System.out.println(10D/3);
+//        System.out.println(10D/3);
+//        System.out.println(10.0/3);
 
-        int a = 10;
-        int b = 3;
+//        int a = 10;
+//        int b = 3;
+//        System.out.println(a%b);
+//        System.out.println(10%1);
+//        System.out.println(10%2);
+//        System.out.println(10%3);
+//        System.out.println(10%4);
+//        System.out.println(10%5);
+//        System.out.println(10%6);
+//        System.out.println(10%7);
+//        System.out.println(10%8);
+//        System.out.println(10%9);
+//        System.out.println(10%10);
+//
+//        System.out.println(2%2);
+//        System.out.println(4%2);
+//        System.out.println(6%2);
+//        System.out.println(8%2);
+//        int a = 10;
+//        int b = 4;
+//        a= a+5;
+//        a+=5;
+//        a*=5;
+//        a/=5;
+//        a%=5;
+//        System.out.println(a);
+//        int a = 5;
+////        a++;
+////        a--;
+//        int b = 10;
+//        int sum = ++a + a++ -b-- + ++b;
+//        System.out.println(sum);
+//        System.out.println(a);
+//        System.out.println(b);
 
-        System.out.println((double) a/b);
-        System.out.println((double) a/(double)b);
-        System.out.println(a/(double)b);
-        System.out.println();
+
+
+//            int b = 2000;
+//            int c = 3500;
+//
+//            int countMonth10Years = 12*10;
+//
+//            int sal1 = a*countMonth10Years;
+//            int sal2 = b*countMonth10Years;
+//            int sal3 = c*countMonth10Years;
+//
+//        System.out.println(sal1);
+//        System.out.println(sal2);
+//        System.out.println(sal3);
+//
+//        double tax = 0.05;
+//
+//        double sal1WithoutTax = sal1 - sal1 * tax;
+//        double sal2WithoutTax = sal2 - sal2 * tax;
+//        double sal3WithoutTax = sal3 - sal3 * tax;
+//
+//        System.out.println(sal1WithoutTax);
+//        System.out.println(sal2WithoutTax);
+//        System.out.println(sal3WithoutTax);
+//
+//        double sum = sal1WithoutTax+sal2WithoutTax+sal3WithoutTax;
+//        double avg = sum / 3;
+//
+//        System.out.println(sum);
+//        System.out.println(avg);
+//
+//        System.out.println(Math.E);
+//        System.out.println(Math.PI);
+//
+//        System.out.println(Math.abs(5));
+//        System.out.println(Math.abs(-5));
+//
+//        System.out.println(Math.pow(5, 3));
+//        System.out.println(Math.pow(2, 2));
+//        System.out.println(Math.pow(3, 4));
+//
+//        System.out.println(Math.sqrt(16));
+//        System.out.println(Math.sqrt(9));
+//
+//        System.out.println(Math.min(10, 20));
+//        System.out.println(Math.max(10, 20));
+//
+//        System.out.println(Math.floor(10.6));
+//        System.out.println(Math.floor(10.4));
+//
+//        System.out.println(Math.round(10.50));
+//        System.out.println(Math.round(10.49999));
+
+//        System.out.println(Math.random()); // [0.0 - 1)
+//        System.out.println((int)Math.random()*10); // [0.0 - 9)
+//        System.out.println(Math.round(Math.random()*10));  // [0.0 - 10)
+
+        //[15, 30]
+//        int min = 15;
+//        int max = 30;
+//        System.out.println((int) (Math.random() * (max - min + 1)) + min);
+
+        //[27, 77]
+        int min = 27;
+        int max = 77;
+        System.out.println((int)(Math.random()*(max-min+1))+min);
 
 
 
